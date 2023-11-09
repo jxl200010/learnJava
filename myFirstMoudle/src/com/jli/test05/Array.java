b7865563a487f723682ceb2c97cf6f19e7c14d05
Jun Li <jaydenli99@hotmail.com>
2023-11-09 01:28:47 -0600

    Class Array Added on 11/9/2023

--- myFirstMoudle/src/com/jli/test05/Array.java
+++ myFirstMoudle/src/com/jli/test05/Array.java
@@ -2,6 +2,15 @@ package com.jli.test05;
 
 public class Array {
     public static void main(String[] args) {
-        System.out.println("git");
+        int[] arr = new int[4];
+
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+
+        for (int num : arr) {
+            System.out.println(num);
+        }
     }
 }
